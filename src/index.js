be9f651eeeded46d53458949b7e72e7d18a5c445
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-30 11:10:37 +0100

    Finish Auth0Provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-iz2udir2ctflxlgq.us.auth0.com"
+      clientId="pSyVN0LMT1qeRGq5LUDxYTljxo0CXK4Z"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
