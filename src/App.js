5060e238176b001babbae01272405adf78f729e0
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-05 10:42:27 +0100

    browseRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Dashboard, Error, Login, PrivateRoute, AuthWrapper } from "./pages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
